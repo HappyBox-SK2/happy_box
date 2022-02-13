f945eaba2f1ed464292c07aeb56a55677e449386
insomenia-yonggyun <dvirusss@insomenia.com>
2022-02-13 19:43:46 +0900

    [#1] next, tailwind setting

---
+++ frontend/components/header.tsx
@@ -0,0 +1,9 @@
+export const Header: React.FC = () => {
+  console.log("show header");
+
+  return (
+    <div className="border-b shadow">
+      <div className="flex">this is Header.</div>
+    </div>
+  );
+};
