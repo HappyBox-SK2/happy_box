f777d211f86453ac0827122e96f3cb45964a5d98
Seungmin Lim <seungmin9066@gmail.com>
2022-03-05 15:42:34 +0900

    #3 [추가] 임베디드 타입 객체 예시 추가

--- backend/src/main/java/com/happybox/happyboxservice/domain/Address.java
+++ backend/src/main/java/com/happybox/happyboxservice/domain/Address.java
@@ -1,18 +1,19 @@
 package com.happybox.happyboxservice.domain;
 
+import static lombok.AccessLevel.*;
+
 import javax.persistence.Embeddable;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@NoArgsConstructor(access = PROTECTED)
 public class Address { // 값타입 기본예제
 	private String city;
 	private String street;
 	private String zipcode;
-
-	protected Address() {
-	}
 
 	public Address(String city, String street, String zipcode) {
 		this.city = city;
