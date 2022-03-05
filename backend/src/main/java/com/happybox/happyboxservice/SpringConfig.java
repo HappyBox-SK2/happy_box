345ab29b22ce99c494bc09af92b8748327097063
Seungmin Lim <seungmin9066@gmail.com>
2022-03-05 15:45:51 +0900

    변경 사항 없음

--- backend/src/main/java/com/happybox/happyboxservice/SpringConfig.java
+++ backend/src/main/java/com/happybox/happyboxservice/SpringConfig.java
@@ -2,11 +2,6 @@ package com.happybox.happyboxservice;
 
 import org.springframework.context.annotation.Configuration;
 
-@Configuration // 정형화되지 않은 클래스들 클래스빈 등록할 때
+@Configuration
 public class SpringConfig {
-
-	// @Bean
-	// public TimeTraceAop timeTraceAop() {
-	// 	return new TimeTraceAop();
-	// }
 }
